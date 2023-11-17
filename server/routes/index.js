ba3cbd5c7b9cdfeeacd75277fae9e6ee8e9da25a
ntn1999 <nam14nt@gamil.com>
2023-11-17 09:47:22 +0700

    format code

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,25 +1,25 @@
-const subscriberRouter = require('./subscribers');
-const authRouter = require('./auth');
-const sensorRouter = require('./sensor');
-const userRouter = require('./user');
-const deviceRouter = require('./device');
-const reportRouter = require('./report');
+const subscriberRouter = require("./subscribers");
+const authRouter = require("./auth");
+const sensorRouter = require("./sensor");
+const userRouter = require("./user");
+const deviceRouter = require("./device");
+const reportRouter = require("./report");
 //Index of route middleware
 const route = (app) => {
-	//Route middleware subscribers
-	app.use('/subscribers', subscriberRouter);
+  //Route middleware subscribers
+  app.use("/subscribers", subscriberRouter);
 
-	//Route middleware auth
-	app.use('/api/auth', authRouter);
+  //Route middleware auth
+  app.use("/api/auth", authRouter);
 
-	//Route sensor
-	app.use('/api/sensor', sensorRouter);
+  //Route sensor
+  app.use("/api/sensor", sensorRouter);
 
-	app.use('/api/user', userRouter);
+  app.use("/api/user", userRouter);
 
-	app.use('/api/device', deviceRouter);
+  app.use("/api/device", deviceRouter);
 
-	app.use('/api/report', reportRouter);
+  app.use("/api/report", reportRouter);
 };
 
 module.exports = route;
